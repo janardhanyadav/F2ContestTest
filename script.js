62a12c30cb69d305e87032ac88f7fb6989b6d171
Janardhan Yadav <deepy125@gmail.com>
2024-02-17 18:25:22 +0530

    First Commit

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(arr => {
+    if (arr.marks > 50) {
+      console.log(`Name: ${arr.name}`);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(arr => {
+    if(arr.marks>50){
+      console.log(`Name: ${arr.name}`);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  const passedStudents = arr.filter(arr => arr.marks >= 50);
+  console.log(passedStudents);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const additionalStudents = [
+    { id: 4, name: "raju", age: "22", marks: 65 },
+    { id: 5, name: "amit", age: "21", marks: 60 },
+    { id: 6, name: "laxmi", age: "25", marks: 70 }
+  ];
+
+  const concatenatedArray = arr.concat(additionalStudents);
+
+  console.log(concatenatedArray);
 }
